78c6fbb40a23efac3edb42c164d0a37f9d6518cb
Rakhimi <anuarrakhimi@gmail.com>
2024-09-24 09:26:07 +0800

    Final3

--- lib/prismadb.ts
+++ lib/prismadb.ts
@@ -1,12 +1,18 @@
 /* eslint no-var: 0 */
 
-import { PrismaClient } from "@prisma/client"
+import { PrismaClient } from '@prisma/client';
 
+// Prevent multiple instances of Prisma Client in development
 declare global {
-  var prisma: PrismaClient | undefined
+  var prisma: PrismaClient | undefined;
 }
 
-const client = globalThis.prisma || new PrismaClient()
-if (process.env.NODE_ENV !== "production") globalThis.prisma = client
+const prisma = global.prisma || new PrismaClient({
+  log: ['query', 'info', 'warn', 'error'],
+});
 
-export default client
+if (process.env.NODE_ENV !== 'production') {
+  global.prisma = prisma;
+}
+
+export default prisma;
